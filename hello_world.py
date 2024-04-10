017c284642decc16f0387150d24f364d0b60621a
matthewcarvery <matthew.carvery@norcal.aaa.com>
2024-04-10 11:51:26 -0400

    Add diff

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,6 @@
 import sys
+from git import Repo
+import os
 
 if __name__== "__main__":
     print ('folder : ', sys.argv[1])
@@ -8,3 +10,32 @@ if __name__== "__main__":
     print(f.read())
 
     print("Hello World!")
+    
+    def git_push(repofolder):
+        try:
+            modifiedFiles = []
+            deletedFiles = []
+            addedFiles = []
+            message = input("Enter Commit Message:\n")
+            repo = Repo(repofolder, search_parent_directories=True)
+            git_root = repo.git.rev_parse("--show-toplevel")
+            repo.git.add(all=True)
+            repo.index.commit(message)
+            previous_commit = repo.commit('HEAD~1')
+            for item in previous_commit.diff('HEAD'):
+                match item.change_type:
+                    case 'M':
+                        modifiedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
+                    case 'A':
+                        addedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
+                    case 'D':
+                        deletedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
+                    case 'R'|'T':
+                        deletedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
+                        addedFiles.append(os.path.normpath(os.path.join(git_root, item.b_path)))
+            return(addedFiles, modifiedFiles, deletedFiles)
+        except:
+            print('Some error occured while pushing the code')   
+        
+    addedFiles, modifiedFiles, deletedFiles = git_push('main')
+    print(modifiedFiles)
