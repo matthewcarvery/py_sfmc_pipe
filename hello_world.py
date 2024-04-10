0316d58b9ef6e68fe1fadc9a36407d55bca1bb07
matthewcarvery <matthew.carvery@norcal.aaa.com>
2024-04-10 15:17:48 -0400

    arg3

--- hello_world.py
+++ hello_world.py
@@ -5,41 +5,9 @@ import os
 if __name__== "__main__":
     print ('folder : ', sys.argv[1])
     print ('BU : ', sys.argv[2])
+    print ('Changes : ', sys.argv[3])
 
-    f = open("home/runner/work/Action_Test/Action_Test/test.htm", "r")
+    f = open("main/test.htm", "r")
     print(f.read())
 
-    print("Hello World!")
-    
-    def git_push(repofolder):
-        try:
-            modifiedFiles = []
-            deletedFiles = []
-            addedFiles = []
-            repo = Repo(repofolder, search_parent_directories=True)
-            git_root = repo.git.rev_parse("--show-toplevel")       
-            previous_commit = repo.commit('HEAD~1')
-            current_commit = repo.commit('HEAD')
-            git_diff = previous_commit.diff(current_commit)
-            changed_files = [ f.b_path for f in git_diff ]
-            print( "\n".join( changed_files ))
-            '''
-            for item in previous_commit.diff('HEAD'):
-                match item.change_type:
-                    case 'M':
-                        modifiedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
-                    case 'A':
-                        addedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
-                    case 'D':
-                        deletedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
-                    case 'R'|'T':
-                        deletedFiles.append(os.path.normpath(os.path.join(git_root, item.a_path)))
-                        addedFiles.append(os.path.normpath(os.path.join(git_root, item.b_path)))
-         
-            return(addedFiles, modifiedFiles, deletedFiles)
-            '''
-        except:
-            print('Some error occured while pushing the code')   
-        
-    git_push('main')
-    
+    print("Hi there!")
