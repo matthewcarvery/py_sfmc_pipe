79dd1c4858a906b22ed5b9aa5ad98faee65a4d2a
matthewcarvery <matthew.carvery@norcal.aaa.com>
2024-04-11 12:30:57 -0400

    print mods

---
+++ SFMC_Pipe.py
@@ -0,0 +1,22 @@
+import sys
+from git import Repo
+import os
+import json
+
+if __name__== "__main__":
+
+    folder = sys.argv[1]
+    BU = sys.argv[2]
+ 
+    adds = json.loads(sys.argv[3])
+    mods = json.loads(sys.argv[4])
+    dels = json.loads(sys.argv[5])
+    rens = json.loads(sys.argv[6])
+    type = json.loads(sys.argv[7])
+
+    
+    print(mods)
+
+
+    f = open("main" + os.sep + mods[0], "r")
+    print(f.read())
