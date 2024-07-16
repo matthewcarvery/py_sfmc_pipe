2306fb2dfa44a093f851069093d1cf97ea44ac07
Matthew Carvery <131385933+matthewcarvery@users.noreply.github.com>
2024-07-16 16:51:47 -0400

    Update SFMC_Pipe.py

--- SFMC_Pipe.py
+++ SFMC_Pipe.py
@@ -28,7 +28,8 @@ if __name__== "__main__":
    repofolder = "main" + os.sep
    accountname = sys.argv[2]
    masterfolder = sys.argv[1]
-   addedFiles = json.loads(sys.argv[3])
+   print(sys.argv[3])
+   #addedFiles = json.loads(sys.argv[3])
    modifiedFiles = json.loads(sys.argv[4])
    deletedFiles = json.loads(sys.argv[5])
    rens = json.loads(sys.argv[6].replace('\\"','\"'))
