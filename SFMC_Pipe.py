2a60276d7b0c4e8beb1b6a707d544a28118718de
Matthew Carvery <131385933+matthewcarvery@users.noreply.github.com>
2024-07-19 13:10:56 -0400

    Update SFMC_Pipe.py

--- SFMC_Pipe.py
+++ SFMC_Pipe.py
@@ -37,11 +37,11 @@ if __name__== "__main__":
     file_list = os.listdir(cur_dir)
     parent_dir = os.path.dirname(cur_dir)
     if file_name in file_list:
-        print "File Exists in: ", cur_dir
+        print("File Exists in: ", cur_dir)
         break
     else:
         if cur_dir == parent_dir: #if dir is root dir
-            print "File not found"
+            print("File not found")
             break
         else:
             cur_dir = parent_dir   
